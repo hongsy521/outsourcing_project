dc0fd18c4ef92350d850f3c014644107f12a0166
hongsy521 <seoyeong0521@naver.com>
2024-07-03 20:31:54 +0900

    feat : post like 약간 수정

--- src/main/java/com/sparta/easyspring/postlike/repository/inf/PostLikeRepositoryInterface.java
+++ src/main/java/com/sparta/easyspring/postlike/repository/inf/PostLikeRepositoryInterface.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostLikeRepositoryInterface extends JpaRepository<PostLike, Long> {
-    PostLike findByUserAndPost(User user, Post post);
+    // PostLike findByUserAndPost(User user, Post post);
 }
